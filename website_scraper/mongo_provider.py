c6b320e6b829a4dc93f8e571d8e3c9b7ea1dd2db
mena-hub <mssz.nnia@gmail.com>
2021-02-07 01:21:59 -0300

    Extract the MongoDB connection logic from pipeline

---
+++ website_scraper/mongo_provider.py
@@ -0,0 +1,16 @@
+import pymongo 
+
+class MongoProvider:
+
+    collection_name = 'andreaga_posts'
+
+    def __init__(self, uri, database):
+        self.mongo_uri = uri
+        self.mongo_db = database or 'website_scraper'
+
+    def get_collection(self):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        return self.client[self.mongo_db][self.collection_name]
+
+    def close_connection(self):
+        self.client.close()
