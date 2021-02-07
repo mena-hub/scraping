f19175818a0e43aa0b4ef71c74e3ea15aa65a57c
mena-hub <mssz.nnia@gmail.com>
2021-02-06 23:59:54 -0300

    Scrape the list pages

--- website_scraper/spiders/andreaga.py
+++ website_scraper/spiders/andreaga.py
@@ -1,10 +1,17 @@
 import scrapy
 
-
 class AndreagaSpider(scrapy.Spider):
     name = 'andreaga'
-    allowed_domains = ['andreaga.com/blog']
     start_urls = ['http://andreaga.com/blog/']
 
     def parse(self, response):
+        for entry in response.css(".hentry"):
+            title = entry.css(".entry-title")
+            url = title.css("::attr(href)").extract_first()
+            yield scrapy.Request(url, callback=self.parse_entry)
+        if response.css(".nav-previous>a"):
+            prev_page_url = response.css(".nav-previous>a::attr(href)").extract_first()
+            yield scrapy.Request(prev_page_url)
+    
+    def parse_entry(self, response):
         pass
