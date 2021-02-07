3600ed56596c98333a11ff42ebb39af0e50b6e34
mena-hub <mssz.nnia@gmail.com>
2021-02-07 00:13:08 -0300

    Add a pages limit argument

--- website_scraper/spiders/andreaga.py
+++ website_scraper/spiders/andreaga.py
@@ -1,8 +1,16 @@
 import scrapy
+import re
 
 class AndreagaSpider(scrapy.Spider):
     name = 'andreaga'
     start_urls = ['http://andreaga.com/blog/']
+
+    def __init__(self, limit_pages=None, *args, **kwargs):
+        super(AndreagaSpider, self).__init__(*args, **kwargs)
+        if limit_pages is not None:
+            self.limit_pages = int(limit_pages)
+        else:
+            self.limit_pages = 0
 
     def parse(self, response):
         for entry in response.css(".hentry"):
@@ -11,7 +19,10 @@ class AndreagaSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_entry)
         if response.css(".nav-previous>a"):
             prev_page_url = response.css(".nav-previous>a::attr(href)").extract_first()
-            yield scrapy.Request(prev_page_url)
+            match = re.match(r".*\/page\/(\d+)\/", prev_page_url)
+            prev_page_number = int(match.groups()[0])
+            if prev_page_number <= self.limit_pages:
+                yield scrapy.Request(prev_page_url)
     
     def parse_entry(self, response):
         pass
